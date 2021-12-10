de39cab69b644cf76d1e1d200e485667c2c5c4bb
chenfwulei@gmail.com <chenfwulei@gmail.com>
2021-12-10 12:10:20 +0800

    init

--- docs/random.sh
+++ docs/random.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-head -c 200 /dev/urandom | tr -dc a-z0-9 | head -c 16
+#head -c 200 /dev/urandom | tr -dc a-z0-9 | head -c 16
+#生成文件id
+namelist=($(find . -type f -name "*.md" |awk -F'/' '{print$NF}'))
+for list in ${namelist[@]};
+do
+	echo -n "$list|" >> filelist.txt 
+	echo $list|md5sum|awk '{print$1}' >> filelist.txt 
+done
